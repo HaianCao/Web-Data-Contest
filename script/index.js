195c16004720e92fa6327ed5b6401b2d456b326e
HaianCao <caohaian2005@gmail.com>
2024-12-25 23:54:13 +0700

    feature change language for faq.html and sponsor.html, modified link in nav-bar to prizes.html

--- script/index.js
+++ script/index.js
@@ -3,11 +3,3 @@
 
     Trong truong hop muon them script thi tao them file JS moi.
 */
-document.onreadystatechange = function() {
-    document.querySelector("body").style.visibility = "hidden";
-    document.querySelector("#loader").style.visibility = "visible";
-    setTimeout(() => {
-        document.querySelector("#loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }, 500)
-};
